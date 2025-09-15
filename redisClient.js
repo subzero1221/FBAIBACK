7afb4a78f162c8f7bb2145b30da36f8d175c2f8e
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-09-15 22:23:13 +0400

    Update redisClient.js
    tls set to true

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,11 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false
+  }
 });
 
 redisClient.on("error", (err) => console.error("Redis error:", err));
