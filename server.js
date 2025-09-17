85756e0235c9c5f0269a71fe8af4798854d288f0
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-09-17 23:59:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
   "https://fbai-git-main-subzero1221s-projects.vercel.app",
   "http://localhost:3000",
   "https://fbai-34o0u1189-subzero1221s-projects.vercel.app",
-  "https://fbai-o40vpmtbz-subzero1221s-projects.vercel.app"
+  "https://fbai-o40vpmtbz-subzero1221s-projects.vercel.app",
+  "https://fbai-53phn9xr9-subzero1221s-projects.vercel.app"
 ];
 
 app.use(
