2d4206c463ca797b9ecd8418b8b6134daff3452f
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-09-19 23:39:29 +0400

    Update server.js

--- server.js
+++ server.js
@@ -20,12 +20,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://fbai-53phn9xr9-subzero1221s-projects.vercel.app",
   "https://fbai-md2qc3nw9-subzero1221s-projects.vercel.app",
-  "https://fbai-34o0u1189-subzero1221s-projects.vercel.app",
-  "https://fbai-eouf2l6ta-subzero1221s-projects.vercel.app",
-  "https://fbai-fxcha8c9k-subzero1221s-projects.vercel.app",
-  "https://fbai-pcse7vdp2-subzero1221s-projects.vercel.app",
-  "https://fbai-huty1x2wj-subzero1221s-projects.vercel.app",
-  "https://fbai-m29q1097v-subzero1221s-projects.vercel.app"
+  "https://fbai-4i7zhpeeu-subzero1221s-projects.vercel.app"
 ];
 
 app.use(
